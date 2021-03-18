d78a9483ccc42491a9c2cfdeb25ddfd0b8e01c47
Tomás G. Martínez <gmatomas@gmail.com>
2021-03-18 20:50:07 -0300

    Fix carpetas

---
+++ A889854.Actividad01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A889854.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+            Console.ReadKey();
+        }
+    }
+}
